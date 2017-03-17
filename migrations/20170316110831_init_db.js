73c0ebaa16ae44ecfb0c271ca4c9abcb550d0941
George Badu <quophyie@yahoo.com>
2017-03-17 14:04:26 +0000

    Added Standard and ESLint Config as well as quantal-base-model and quantal-errors in addition to bookshelf and knex config

---
+++ migrations/20170316110831_init_db.js
@@ -0,0 +1,35 @@
+'use strict'
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+      knex.schema.createTableIfNotExists('users', (table) => {
+          table.increments()
+          table.string('username')
+          table.string('password')
+          table.string('first_name')
+          table.string('last_name')
+          table.string('gender', 1)
+          table.string('email', 128)
+          table.timestamp('dob')
+          table.timestamp('deactived_date').defaultTo(knex.fn.now())
+          table.timestamps()
+          //table.timestamps(true, true)
+
+      }),
+      knex.schema.createTableIfNotExists('giphys', (table) => {
+          table.increments('giphy_id')
+          table.string('query').unique()
+          table.string('url')
+          // table.timestamps()
+          table.timestamps(true, true)
+
+      })
+  ])
+}
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfExists('users'),
+        knex.schema.dropTableIfExists('giphys')
+    ])
+}
